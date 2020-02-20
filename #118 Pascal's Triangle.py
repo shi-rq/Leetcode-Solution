288143aed01f7f37dea88021674e592879c976d6
shi-rq <shi.rq.ai@gmail.com>
2020-02-20 23:42:01 +0800

    Update #118 Pascal's Triangle.py

--- #118 Pascal's Triangle.py
+++ #118 Pascal's Triangle.py
@@ -17,6 +17,7 @@ Output:
 '''
 
 
+# 1 Iteration
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         if numRows == 0: return []
@@ -29,3 +30,33 @@ class Solution:
             level += [1]
             triangle += [level]
         return triangle
+
+
+# 2 Iteration (Mathematics)
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+        for n in range(numRows):
+            level = []
+            for m in range(n+1):
+                num = 1
+                for p in range(m):
+                    num *= n-p
+                    num /= p+1
+                level.append(int(num))
+            triangle.append(level)
+        return triangle
+
+
+# 3 Recursion
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0: return []
+        else:
+            small_triangle = self.generate(numRows - 1)
+            triangle = [[1]] + self.generate(numRows - 1)
+            for n in range(len(small_triangle)):
+                for m in range(len(small_triangle[n]) - 1):
+                    triangle[n+1][m+1] += small_triangle[n][m]
+                triangle[n+1] += [1]
+            return triangle

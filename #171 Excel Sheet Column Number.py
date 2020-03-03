e2f3684ba731e5c1440bdc77c1a90a464a0f8ba5
shi-rq <shi.rq.ai@gmail.com>
2020-03-03 18:44:34 +0800

    Add files via upload

---
+++ #171 Excel Sheet Column Number.py
@@ -0,0 +1,49 @@
+'''
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+    ...
+Example 1:
+
+Input: "A"
+Output: 1
+Example 2:
+
+Input: "AB"
+Output: 28
+Example 3:
+
+Input: "ZY"
+Output: 701
+'''
+
+
+# 1 Iteration
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        digit = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+        num = 0
+        for n in range(len(s)):
+            num += 26 ** n * digit[s[len(s)-1-n]]
+        return num
+
+
+# 2 Recursion
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        digit = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12,
+                 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23,
+                 'X': 24, 'Y': 25, 'Z': 26}
+
+        if len(s) == 1:
+            return digit[s]
+        else:
+            return self.titleToNumber(s[:-1]) * 26 + digit[s[-1]]
